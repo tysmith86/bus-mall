875333eb84ac393e23ce7dcbc41cfbc6abe71801
Tyler <tyler.smith6891@gmail.com>
2016-09-01 06:39:31 -0700

    added comments

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ var images = [];
 var currentImageIndices = [0, 1, 2];
 var totalClicks = 0;
 
-// get local storage, then check the length of the array
-
-// var jsonImages = [];
+// checks the length of local storage
+// if there is something in local storage, set the images array equal to that parsed value
+// if there is nothing in local storage, generate the image array from scratch
 if (localStorage.length === 1) {
   images = JSON.parse(localStorage.getItem('jsonImages'));
 } else {
@@ -20,7 +20,7 @@ if (localStorage.length === 1) {
   }
 }
 
-
+// Img constructor
 function Img(name, path) {
   this.views = 0;
   this.clicks = 0;
@@ -39,18 +39,21 @@ drawImage(0);
 drawImage(1);
 drawImage(2);
 
+// refreshes page
+var resetButton = document.getElementById('restart');
 
-var resetButton = document.getElementById('clear_storage');
-
+// clickHandler for image clicks
 function clickHandler(event) {
   // console.log('Event Target:', event.target);
   var matchPath = event.target.getAttribute('src');
+  // ends the survey at 25 clicks, and removes the hidden class from the buttons
   if (totalClicks >= 25) {
     var chartButton = document.getElementById('show_chart');
     chartButton.setAttribute('class', '');
     resetButton.setAttribute('class', '');
     return;
   }
+  // if area outside of pictures, but inside ul is clicked, the function short circuits
   if(!matchPath) {
     return;
   }
